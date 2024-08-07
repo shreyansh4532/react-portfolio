787ed8073688fdb268b5752d8888a2036f25ed07
Shreyansh Mishra <shreyanshmishracr7@gmail.com>
2024-08-07 10:37:30 +0530

    updated navbar content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,31 @@ import logo from '../assets/kevinRushLogo.png'
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithubAlt } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import { SiLeetcode } from "react-icons/si";
 
 const Navbar = () => {
+  function handleGithubClick() {
+    window.open('https://github.com/shreyansh4532', '_blank');
+  }
+
+  function handleLeetcodeClick() {
+    window.open('https://leetcode.com/u/shreyanshmishracr7/', '_blank'); 
+  }
+
+  function handleLinkedInClick() {
+    window.open('https://www.linkedin.com/in/shreyansh-mishra-rmfc/', '_blank'); 
+  }
+
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
             <img className='w-10 mx-2' src={logo} alt="logo image" />
         </div>
         <div className='flex gap-4 m-8 items-center justify-center text-2xl'>
-            <FaLinkedinIn />
-            <FaGithubAlt />
+            <FaLinkedinIn onClick={handleLinkedInClick} className='cursor-pointer'/>
+            <FaGithubAlt onClick={handleGithubClick} className='cursor-pointer'/>
+            <SiLeetcode onClick={handleLeetcodeClick} className='cursor-pointer'/>
             <FaInstagram />
-            <FaXTwitter />
         </div>
     </nav>
   )
